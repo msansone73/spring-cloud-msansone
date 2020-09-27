77189562b02ac3625cd2f63f706815a816d542d3
marcio sansone <msansone@gmail.com>
2020-09-27 12:01:33 +0100

    just empty projects

---
+++ client-service/src/test/java/br/com/msansone/api/clientservice/ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.msansone.api.clientservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ zuul-service/src/main/java/br/com/msansone/api/zuulservice/ZuulServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.msansone.api.zuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServiceApplication.class, args);
+	}
+
+}
