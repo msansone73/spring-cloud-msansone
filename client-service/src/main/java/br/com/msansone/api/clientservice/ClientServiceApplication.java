9d23b023af8beea1cc9372b949783e3e742fc12b
marcio sansone <msansone@gmail.com>
2020-09-27 16:37:16 +0100

    added getByLogin creates in security-service

--- client-service/src/main/java/br/com/msansone/api/clientservice/ClientServiceApplication.java
+++ client-service/src/main/java/br/com/msansone/api/clientservice/ClientServiceApplication.java
@@ -2,8 +2,10 @@ package br.com.msansone.api.clientservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ClientServiceApplication {
 
 	public static void main(String[] args) {
